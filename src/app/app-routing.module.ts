65890e804c7b03301944882c077fd52157f13fa3
miniduiit <minidu.2019321@iit.ac.lk>
2023-09-07 21:14:58 +0530

    init commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LayoutComponent} from "./features/layout/layout.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', pathMatch:'full', component: LayoutComponent},
+  {path: 'layout', component: LayoutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
